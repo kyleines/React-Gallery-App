8753b23e7b3d32d9c89d6163a9b26f7f640b2ad2
kyleines <kyleines@gmail.com>
2021-06-15 09:10:47 -0500

    import Gallery in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, {Component} from "react";
 
-const App = () => (
-    <h1>Hello World</h1>
-);
+// API Key
+import myKey from "../config.js";
+
+// App components
+import Gallery from "./Gallery";
+
+
+class App extends Component {
+
+    state = {
+        photos: []
+    }
+
+    componentDidMount() {
+        this.getPhotos();
+    }
+
+    getPhotos = () => {
+        const apiKey = myKey;
+        fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=kitten&per_page=24&format=json&nojsoncallback=1`)
+            .then(response => response.json())
+            .then(responseData => {
+                this.setState({
+                    photos: responseData.photos.photo
+                });
+            })
+            .catch(error => {
+                console.log("Error fetching and parsing data: ", error);
+            });
+    }
+
+    render() {
+        return (
+            <Gallery photos={this.state.photos} />
+        );
+    }
+};
 
 export default App;
