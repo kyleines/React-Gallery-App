7df6ea83c49167ad94a0d2392957837b95e9e27f
kyleines <kyleines@gmail.com>
2021-06-17 03:36:19 -0500

    finalize main container component

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,59 @@
-import React, {Component} from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {
+    BrowserRouter,
+    Route,
+    Switch,
+    Redirect
+} from "react-router-dom";
+
+// App components
+import Search from "./Search";
+import Nav from "./Nav";
+import Gallery from "./Gallery";
+import Error from "./Error";
 
 // API Key
 import myKey from "../config.js";
+const apiKey = myKey;
 
-// App components
-import Gallery from "./Gallery";
+const App = () => {
 
+    const [photos, setPhotos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-class App extends Component {
-
-    state = {
-        photos: []
-    }
-
-    componentDidMount() {
-        this.getPhotos();
-    }
-
-    getPhotos = () => {
-        const apiKey = myKey;
-        fetch(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=kitten&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => response.json())
-            .then(responseData => {
-                this.setState({
-                    photos: responseData.photos.photo
-                });
+    const getPhotos = (query) => {
+        document.title = `Photos of ${query}`;
+        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+            .then(response => {
+                setPhotos(response.data.photos.photo);
+                setLoading(false);
             })
             .catch(error => {
                 console.log("Error fetching and parsing data: ", error);
             });
     }
 
-    render() {
-        return (
-            <Gallery photos={this.state.photos} />
-        );
-    }
-};
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <h1>React Gallery App</h1>
+                <Search getPhotos={getPhotos} />
+                <Nav />
+                <Switch>
+                    <Redirect exact from="/" to="/cats" />
+                    <Route exact path="/:query">
+                        <Gallery 
+                            loading={loading}
+                            photos={photos}
+                            getPhotos={getPhotos} 
+                        />
+                    </Route>
+                    <Route component={Error} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    );
+}
 
 export default App;
