58ead1e7653cd5bca72daf3011110624ad70736b
kyleines <kyleines@gmail.com>
2021-06-17 03:16:04 -0500

    add Error.js component

---
+++ src/components/Error.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Error = () => {
+    document.title = `404`;
+    return (
+        <div className="photo-container">
+            <div className="not-found">
+                <hr />
+                <h2>404</h2>
+                <p>The page you are trying to view does not exist. 
+                <br />
+                Please select a topic above or try the search bar.</p>
+                <hr />
+                <br />
+            </div>
+        </div>
+    )
+};
+
+export default Error;
