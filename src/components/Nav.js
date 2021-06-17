83312f67f335af2b9ddc06fa3f261caa8245c436
kyleines <kyleines@gmail.com>
2021-06-17 03:28:11 -0500

    add NavLinks to Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {NavLink} from "react-router-dom";
+
+const Nav = () => {
+    return (
+        <nav className="main-nav">
+            <ul>
+            <li><NavLink exact to="/cats">Cats</NavLink></li>
+            <li><NavLink exact to="/dogs">Dogs</NavLink></li>
+            <li><NavLink exact to="/Bo%20Burnham">Bo</NavLink></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
